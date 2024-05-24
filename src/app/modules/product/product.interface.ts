e58aac933bf4459089732dbb0e19f36ca631342d
Ubaidullah-Hasan <hm2964133@gmail.com>
2024-05-24 11:43:52 +0600

    interface and model create

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,16 @@
+export type TProduct = {
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+    tags: string[];
+    variants: {
+        type: string;
+        value: string;
+    }[];
+    inventory: {
+        quantity: number;
+        inStock: boolean;
+    };
+};
+
---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+import { TProduct } from "./product.interface";
+
+const productSchema = new Schema<TProduct>({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    tags: { type: [String], required: true },
+    variants: {
+        type: [
+            {
+                type: { type: String, required: true },
+                value: { type: String, required: true },
+            },
+        ],
+        required: true,
+    },
+    inventory: {
+        quantity: { type: Number, required: true },
+        inStock: { type: Boolean, required: true },
+    },
+});
+
+
+export const ProductModel = mongoose.model('Product', productSchema);
