8c9becabf8aa7950605e6c03249b1278cf81d800
Ubaidullah-Hasan <hm2964133@gmail.com>
2024-05-24 23:01:19 +0600

    quantity and stock functionality added

--- src/app/modules/order/order.services.ts
+++ src/app/modules/order/order.services.ts
@@ -1,10 +1,32 @@
 import { OrderModel } from "./order.model";
 import { TOrder } from "./order.interface";
+import { Types } from "mongoose";
+import { ProductModel } from "../product/product.model";
 
 const createOrderIntoDB = async (orderData: TOrder) => {
-  const result = await OrderModel.create(orderData);
-  return result;
+  const objectId = new Types.ObjectId(orderData.productId);
+  const product = await ProductModel.findById(objectId);
+  if (!product) {
+    throw new Error("Product ID does not exist");
+  }
+
+  if (product.inventory.quantity < orderData.quantity) {
+    throw new Error("Not enough quantity in stock");
+  }
+
+  const newOrder = new OrderModel(orderData);
+  await newOrder.save();
+
+  product.inventory.quantity -= orderData.quantity;
+  if (product.inventory.quantity === 0) {
+    product.inventory.inStock = false;
+  }
+
+  await product.save();
+
+  return newOrder;
 };
+
 const getOrderDataFromDB = async () => {
   const result = await OrderModel.find({});
   if (result.length === 0) {
